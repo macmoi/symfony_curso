b6d17253c8c832ebb3e0e1e586a96f68b28f339e
Moises <moises.pinate@outlook.com>
2020-01-16 22:25:24 -0400

    Agregamos plantilla twig para posts

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,20 +2,40 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PostController {
+/**
+ * Spanish (Native Language)
+ *
+ * Esta clase es para practicar cosas de symfony.
+ *
+ * Extendemos de AbstractController porque en dicha clase padre disponemos de
+ * metodos ultiles para render de plantillas y demas acciones...
+ *
+ * @author Moises Piñate <moises.pinate@outlook.com>
+ */
+class PostController extends AbstractController {
 
     /**
      * I've used the route annotation for indicate the matching url to this method
      *
      * @Route("post/home")
      */
-    public function home() {
+    public function home()
+    {
         $mName = 'Pruebas';
-        return new Response(
-            "<html><body><h1>$mName</h1></body></html>"
-        );
+        return new Response($mName);
+    }
+
+    /**
+     * @Route("blog/{post}")
+     */
+    public function showPost($post)
+    {
+        return $this->render('posts/post.html.twig', [
+            'title' => ucwords(str_replace('-', '', $post))
+        ]);
     }
 }
